c31bd795dfa696d3f8622a2e12fc37770e3ecaa9
angelip2303 <angel.iglesias.prestamo@gmail.com>
2023-01-13 13:43:18 +0100

    build?

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const webpackConfig = require("./webpack.config");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(webpackConfig, {
   mode: "production",
